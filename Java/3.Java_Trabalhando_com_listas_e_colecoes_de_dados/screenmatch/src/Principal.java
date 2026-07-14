ca7eaedc02de07f8013f0d9265a744d6deccea74
ReiSantana <rei.santana@gmail.com>
2026-07-14 18:19:55 -0300

    Início do curso 3 -> Java: trabalhando com listas e coleções

---
+++ Java/3.Java_Trabalhando_com_listas_e_colecoes_de_dados/screenmatch/src/Principal.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+
+        System.out.println("**---------------------------------------------**");
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(175);
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        meuFilme.exibeFichaTecnica();
+        System.out.println("Classificação do filme \"" + meuFilme.getNome() + "\": ");
+        filtro.filtra(meuFilme);
+
+        System.out.println("**---------------------------------------------**");
+        // Testando a classe Serie
+        Serie minhaSerie = new Serie();
+        minhaSerie.setNome("Breaking Bad");
+        minhaSerie.setAnoDeLancamento(2008);
+        minhaSerie.setIncluidoNoPlano(true);
+        minhaSerie.setAtiva(true);
+        minhaSerie.setTemporadas(5);
+        minhaSerie.setEpisodiosPorTemporada(62);
+        minhaSerie.setMinutosPorEpisodio(47);
+
+        minhaSerie.avalia(9);
+        minhaSerie.avalia(10);
+        minhaSerie.avalia(8);
+        minhaSerie.avalia(7);
+
+        minhaSerie.exibeFichaTecnica();
+        Episodio episodio1 = new Episodio();
+        episodio1.setNumero(1);
+        episodio1.setNome("Pilot");
+        episodio1.setSerie(minhaSerie);
+        episodio1.setTotalVisualizacoes(300);
+        System.out.println("Classificação do episódio \"" + episodio1.getNome() + "\": ");
+        filtro.filtra(episodio1);
+
+        System.out.println("**---------------------------------------------**");
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("A Ilha do Medo");
+        outroFilme.setAnoDeLancamento(1972);
+        outroFilme.setDuracaoEmMinutos(120);
+        outroFilme.avalia(9.5);
+        outroFilme.avalia(10);
+        outroFilme.avalia(10);
+
+        outroFilme.exibeFichaTecnica();
+
+        System.out.println(
+                "**----------------------------------------------------------------------------------------**");
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+
+        calculadora.inclui(meuFilme);
+        System.out.println(
+                "Tempo total (com \"" + meuFilme.getNome() + "\"): " + calculadora.getTempoTotal() + " minutos");
+
+        calculadora.inclui(minhaSerie);
+        System.out.println(
+                "Tempo total (com \"" + minhaSerie.getNome() + "\"): " + calculadora.getTempoTotal() + " minutos");
+
+        calculadora.inclui(outroFilme);
+        System.out.println(
+                        "Tempo total (com \"" + outroFilme.getNome() + "\"): " + calculadora.getTempoTotal()
+                                        + " minutos");
+
+
+        // Aqui começou o curso de Listas e Coleções de Dados
+        var filmeDoPaulo = new Filme();
+        filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setAnoDeLancamento(2003);
+        filmeDoPaulo.setDuracaoEmMinutos(178);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme da lista: " + listaDeFilmes.get(0).getNome());
+
+
+
+    }
+}
---
+++ Java/3.Java_Trabalhando_com_listas_e_colecoes_de_dados/screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,74 @@
+package br.com.alura.screenmatch.modelos;
+public class Titulo {
+
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+ 
+    public void exibeFichaTecnica() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        // double notaMedia = somaDasAvaliacoes / totalDeAvaliacoes;
+        // ou
+        double notaMedia = pegaMedia();
+        System.out.println(String.format("Média das avaliações (%d): %.2f", totalDeAvaliacoes, notaMedia));
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        double media = somaDasAvaliacoes / totalDeAvaliacoes;
+        return media;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public double getSomaDasAvaliacoes() {
+        return somaDasAvaliacoes;
+    }
+
+    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
+        this.somaDasAvaliacoes = somaDasAvaliacoes;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+}
