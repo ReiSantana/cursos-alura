e7d9fa3a8369d4fe31ca70c091013d1544cc11b3
Reinaldo Pereira de Santana <reinaldo.santana@serpro.gov.br>
2026-08-04 17:25:23 -0300

    Atualização parte aula 2 do curso Java: consumindo API, gravando arquivos e lidando com erros

--- Java/4.Java-consumindo_API_gravando_arquivos_e_lidando_com_erros/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ Java/4.Java-consumindo_API_gravando_arquivos_e_lidando_com_erros/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,9 +1,14 @@
+import modelos.Titulo;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Scanner;
+import com.google.gson.Gson;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
 
 public class PrincipalComBusca {
 
@@ -22,6 +27,14 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
         System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println("JSON recebido: " + json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+
+        System.out.println(meuTitulo);
     
     }
 
