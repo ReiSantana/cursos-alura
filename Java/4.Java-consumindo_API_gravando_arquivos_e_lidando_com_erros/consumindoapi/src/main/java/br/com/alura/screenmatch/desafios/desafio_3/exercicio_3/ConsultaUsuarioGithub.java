d18e378ae4f1cfa3f4e70e54818819919b2989f5
Reinaldo Pereira de Santana <reinaldo.santana@serpro.gov.br>
2026-08-18 10:59:06 -0300

    Atualização até o final da aula 3 do curso Java: consumindo API, gravando arquivos e lidando com erros

--- Java/4.Java-consumindo_API_gravando_arquivos_e_lidando_com_erros/consumindoapi/src/main/java/br/com/alura/screenmatch/desafios/desafio_3/exercicio_3/ConsultaUsuarioGithub.java
+++ Java/4.Java-consumindo_API_gravando_arquivos_e_lidando_com_erros/consumindoapi/src/main/java/br/com/alura/screenmatch/desafios/desafio_3/exercicio_3/ConsultaUsuarioGithub.java
@@ -1,5 +1,9 @@
 package br.com.alura.screenmatch.desafios.desafio_3.exercicio_3;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class ConsultaUsuarioGithub {
@@ -11,11 +15,32 @@ public class ConsultaUsuarioGithub {
         String usuario = leTeclado.nextLine();
         leTeclado.close();
 
+        String endereco = "https://api.github.com/users/" + usuario;
+
         try {
-            String resultado = consultarUsuarioGithub(usuario);
-            System.out.println("Resultado da consulta: " + resultado);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .header("Accept", "application/vnd.github.v3+json")
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 404) {
+                throw new ErroConsultaGitHubException("Usuário não encontrado no GitHub.");
+            }
+
+            String json = response.body();
+            System.out.println(json);
+
+            // Adicione aqui o código para processar o JSON e exibir as informações desejadas
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Opss… Houve um erro durante a consulta à API do GitHub.");
+            e.printStackTrace();
         } catch (ErroConsultaGitHubException e) {
-            System.out.println("Erro na consulta ao GitHub: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
 
